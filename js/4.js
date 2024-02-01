ceea9f1a78f3d1ddc88a86af3e98c472d2fb08f5
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2024-02-01 12:49:04 +0000

    change task 4

--- js/4.js
+++ js/4.js
@@ -1,6 +1,7 @@
 /*4 Дано число. Определить, заканчивается оно четной цифрой или нечетной? Вывести последнюю цифру.*/
 let number = parseInt(prompt("Enter integer Number"));
 let numberLastDigit = number % 10;
+console.log(number)
 
 alert(numberLastDigit % 2 === 0 ? `The last digit of your number is ${numberLastDigit}. It is even` : `The last digit of your number is ${numberLastDigit}. It is odd`)
 
